b65c7e9a7bbb401149603a3af5499dadbd3879d5
sfgardev <sf.garol@gmail.com>
2024-02-01 22:00:18 +0700

    added $ sign to FlexWrapper props

--- src/components/FlexWrapper.ts
+++ src/components/FlexWrapper.ts
@@ -1,16 +1,16 @@
 import styled, { CSSProperties } from "styled-components";
 
 type FlexWrapperProps = {
-  direction?: CSSProperties["flexDirection"];
-  align?: CSSProperties["alignItems"];
-  justify?: CSSProperties["justifyContent"];
-  wrap?: CSSProperties["flexWrap"];
+  $direction?: CSSProperties["flexDirection"];
+  $align?: CSSProperties["alignItems"];
+  $justify?: CSSProperties["justifyContent"];
+  $wrap?: CSSProperties["flexWrap"];
 };
 
 export const FlexWrapper = styled.div<FlexWrapperProps>`
   display: flex;
-  flex-direction: ${(props) => props.direction};
-  align-items: ${(props) => props.align};
-  justify-content: ${(props) => props.justify};
-  flex-wrap: ${(props) => props.wrap};
+  flex-direction: ${(props) => props.$direction};
+  align-items: ${(props) => props.$align};
+  justify-content: ${(props) => props.$justify};
+  flex-wrap: ${(props) => props.$wrap};
 `;
