c4862f3ade896ba859d519cd84a6e3991a71422b
sfgardev <sf.garol@gmail.com>
2024-02-01 23:54:23 +0700

    added Badge component

---
+++ src/components/styled/Badge.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Badge = styled.div`
+  width: 5.25rem;
+  padding: 0 1.375;
+  border-radius: 100px;
+  background-color: #d7ffe0;
+  color: #018C0F;
+  font-size: 0.5625rem;
+  line-height: 2.8;
+  font-weight: 600;
+  text-align: center;
+`;
