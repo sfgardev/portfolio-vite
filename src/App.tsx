622ac067d4db67fe4baedd3bc4706ad7c8678f57
sfgardev <sf.garol@gmail.com>
2024-02-02 13:20:23 +0700

    added TechStack section

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import GlobalStyle from "./GlobalStyle";
 import { Header } from "./components/Header/Header";
 import { About } from "./sections/About/About";
 import { Hero } from "./sections/Hero/Hero";
+import { TechStack } from "./sections/TechStack/TechStack";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <main>
         <Hero />
         <About />
+        <TechStack />
       </main>
     </>
   );
---
+++ src/sections/TechStack/TechStack.tsx
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+import { Container } from "../../components/styled/Container";
+import { SectionTitle } from "../../components/styled/SectionTitle";
+import { SubTitle } from "../../components/styled/SubTitle";
+import { FlexWrapper } from "../../components/styled/FlexWrapper";
+import { Icon } from "../../components/Icon/Icon";
+
+const techIcons = [
+  "html",
+  "css",
+  "js",
+  "react",
+  "redux",
+  "bootstrap",
+  "tailwind",
+  "sass",
+  "git",
+  "greensock",
+  "vs-code",
+  "github-tech",
+];
+
+export const TechStack = () => {
+  return (
+    <StyledTechStack>
+      <Container>
+        <FlexWrapper $direction="column" $gap="2rem" $mb="7rem">
+          <SectionTitle $textAlign="center">My Tech Stack</SectionTitle>
+          <SubTitle $textAlign="center">Things I’ve built so far</SubTitle>
+        </FlexWrapper>
+        <FlexWrapper $justify="center" $gap='5.9rem' $wrap="wrap">
+          {techIcons.map((icon) => (
+            <Icon key={icon} iconId={icon} width="120" height="120" />
+          ))}
+        </FlexWrapper>
+      </Container>
+    </StyledTechStack>
+  );
+};
+
+const StyledTechStack = styled.section`
+  padding-top: 12.5rem;
+`;
