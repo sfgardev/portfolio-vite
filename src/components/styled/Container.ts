327e3af1aacbc5ab43fd1b52c6657850a6628f61
sfgardev <sf.garol@gmail.com>
2024-02-02 11:55:08 +0700

    added padding to container

--- src/components/styled/Container.ts
+++ src/components/styled/Container.ts
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  max-width: 74.5rem; // 1192px
+  max-width: 76.5rem; // 1224px
   margin: 0 auto;
-`;
+  padding: 0 1rem;
+`; 
