86131cf772a585eac9bc7cae15fe31f926eccec1
sfgardev <sf.garol@gmail.com>
2024-02-02 13:19:33 +0700

    added prop textAlign

--- src/components/styled/SectionTitle.ts
+++ src/components/styled/SectionTitle.ts
@@ -1,7 +1,12 @@
-import styled from "styled-components";
+import styled, { CSSProperties } from "styled-components";
 
-export const SectionTitle = styled.h2`
+type SectionTitleProps = {
+  $textAlign?: CSSProperties["textAlign"];
+};
+
+export const SectionTitle = styled.h2<SectionTitleProps>`
   font-size: 2.625rem;
   line-height: 1.23;
   color: #42446e;
+  text-align: ${(props) => props.$textAlign};
 `;
