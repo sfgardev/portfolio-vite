add4cb1eb7eb39e947f58f2344b39b7dfebd0abc
sfgardev <sf.garol@gmail.com>
2024-02-02 16:15:47 +0700

    added projects section

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import GlobalStyle from "./GlobalStyle";
 import { Header } from "./components/Header/Header";
 import { About } from "./sections/About/About";
 import { Hero } from "./sections/Hero/Hero";
+import { Projects } from "./sections/Projects/Projects";
 import { TechStack } from "./sections/TechStack/TechStack";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Hero />
         <About />
         <TechStack />
+        <Projects />
       </main>
     </>
   );
---
+++ src/sections/Projects/Projects.tsx
@@ -0,0 +1,72 @@
+import styled from "styled-components";
+import { Container } from "../../components/styled/Container";
+import { FlexWrapper } from "../../components/styled/FlexWrapper";
+import { SectionTitle } from "../../components/styled/SectionTitle";
+import { SubTitle } from "../../components/styled/SubTitle";
+import { ProjectsCard } from "./ProjectsCard";
+
+import project1Jpeg from "../../assets/images/projects/project-1.jpeg";
+import project1Webp from "../../assets/images/projects/project-1.webp";
+import project2Jpeg from "../../assets/images/projects/project-2.jpeg";
+import project2Webp from "../../assets/images/projects/project-2.webp";
+import project3Jpeg from "../../assets/images/projects/project-3.jpeg";
+import project3Webp from "../../assets/images/projects/project-3.webp";
+import project4Jpeg from "../../assets/images/projects/project-4.jpeg";
+import project4Webp from "../../assets/images/projects/project-4.webp";
+import project5Jpeg from "../../assets/images/projects/project-5.jpeg";
+import project5Webp from "../../assets/images/projects/project-5.webp";
+import project6Jpeg from "../../assets/images/projects/project-6.jpeg";
+import project6Webp from "../../assets/images/projects/project-6.webp";
+
+const projectItems = [
+  {
+    jpegImg: project1Jpeg,
+    webpImg: project1Webp,
+  },
+  {
+    jpegImg: project2Jpeg,
+    webpImg: project2Webp,
+  },
+  {
+    jpegImg: project3Jpeg,
+    webpImg: project3Webp,
+  },
+  {
+    jpegImg: project4Jpeg,
+    webpImg: project4Webp,
+  },
+  {
+    jpegImg: project5Jpeg,
+    webpImg: project5Webp,
+  },
+  {
+    jpegImg: project6Jpeg,
+    webpImg: project6Webp,
+  },
+];
+
+export const Projects = () => {
+  return (
+    <StyledProjects>
+      <Container>
+        <FlexWrapper $direction="column" $gap="2rem" $mb="7rem">
+          <SectionTitle $textAlign="center">Projects</SectionTitle>
+          <SubTitle $textAlign="center">Things I’ve built so far</SubTitle>
+        </FlexWrapper>
+        <FlexWrapper $gap="4rem 2.125rem" $wrap="wrap">
+          {projectItems.map((item, index) => (
+            <ProjectsCard
+              key={index}
+              webpUrl={item.webpImg}
+              jpegUrl={item.jpegImg}
+            />
+          ))}
+        </FlexWrapper>
+      </Container>
+    </StyledProjects>
+  );
+};
+
+const StyledProjects = styled.section`
+  padding-top: 12.5rem;
+`;
