377b7a11fed3afebe03ce473961673d85ab2fffd
sfgardev <sf.garol@gmail.com>
2024-02-02 12:34:26 +0700

    added prop fill to Icon component

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -3,12 +3,13 @@ import sprite from "../../assets/sprites.svg";
 type IconProps = {
   width: string;
   height: string;
+  fill?: string;
   iconId: string;
 };
 
-export const Icon = ({ width, height, iconId }: IconProps) => {
+export const Icon = ({ width, height, fill, iconId }: IconProps) => {
   return (
-    <svg width={width} height={height}>
+    <svg width={width} height={height} fill={fill}>
       <use xlinkHref={`${sprite}#${iconId}`} />
     </svg>
   );
