b6d7f1d9f8e0f2a0005136f5387b8375e84f57c4
sfgardev <sf.garol@gmail.com>
2024-02-07 22:55:52 +0700

    added hover to logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -9,4 +9,11 @@ export const Logo = () => {
   );
 };
 
-const StyledLogo = styled.a``;
+const StyledLogo = styled.a`
+  transition: all 0.35s;
+
+  &:hover {
+    filter: brightness(0.7);
+    opacity: 0.8;
+  }
+`;
